2ed7b14041b4494731f56d9aa709f1c7936d27c9
Mikiko Noro <miriks818@gmail.com>
2022-06-09 21:28:00 +0900

    shopCreateDelete

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -5,159 +5,179 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Owner; // Eloquent
+use App\Models\shop;
 use Illuminate\Support\Facades\DB; // QueryBuilder
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Hash;
+use Throwable;
+use Illuminate\Support\Facades\Log;
 
 class OwnersController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+  /**
+   * Display a listing of the resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
 
-    public function __construct()
-    {
-        $this->middleware('auth:admin');
-    }
+  public function __construct()
+  {
+    $this->middleware('auth:admin');
+  }
 
-    public function index()
-    {
-        // $data_now = Carbon::now();
-        // $data_parse = Carbon::now();
-        // echo $data_now->year;
-        // echo $data_parse;
+  public function index()
+  {
+    // $data_now = Carbon::now();
+    // $data_parse = Carbon::now();
+    // echo $data_now->year;
+    // echo $data_parse;
 
-        // $e_all = Owner::all();
-        // $q_get = DB::table('owners')->select('name', 'created_at')->get();
-        // $q_first = DB::table('owners')->select('name')->first();
+    // $e_all = Owner::all();
+    // $q_get = DB::table('owners')->select('name', 'created_at')->get();
+    // $q_first = DB::table('owners')->select('name')->first();
 
-        // $c_test = collect([
-        //     'name' => 'テスト'
-        // ]);
+    // $c_test = collect([
+    //     'name' => 'テスト'
+    // ]);
 
-        // var_dump($q_first);
+    // var_dump($q_first);
 
-        // dd($e_all, $q_get, $q_first, $c_test);
-        $owners = Owner::select('id', 'name', 'email', 'created_at')->paginate(3);
+    // dd($e_all, $q_get, $q_first, $c_test);
+    $owners = Owner::select('id', 'name', 'email', 'created_at')->paginate(3);
 
-        return view('admin.owners.index', compact('owners'));
-    }
+    return view('admin.owners.index', compact('owners'));
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        return view('admin.owners.create');
-    }
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    return view('admin.owners.create');
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:owners',
-            'password' => 'required|string|confirmed|min:8',
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $request->validate([
+      'name' => 'required|string|max:255',
+      'email' => 'required|string|email|max:255|unique:owners',
+      'password' => 'required|string|confirmed|min:8',
+    ]);
+
+    try {
+      DB::transaction(function () use ($request) {
+        $owner = Owner::create([
+          'name' => $request->name,
+          'email' => $request->email,
+          'password' => Hash::make($request->password),
         ]);
-        Owner::create([
-            'name' => $request->name,
-            'email' => $request->email,
-            'password' => Hash::make($request->password),
+
+        Shop::create([
+          'owner_id' => $owner->id,
+          'name' => '店名を入力してください',
+          'information' => '',
+          'filename' => '',
+          'is_selling' => true
         ]);
-        return redirect()
-            ->route('admin.owners.index')
-            ->with([
-                'message' => 'オーナー登録を実施しました。',
-                'status' => 'info'
-            ]);
+      }, 2);
+    } catch (Throwable $e) {
+      Log::error($e);
+      throw $e;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    return redirect()
+      ->route('admin.owners.index')
+      ->with([
+        'message' => 'オーナー登録を実施しました。',
+        'status' => 'info'
+      ]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $owner = Owner::findOrFail($id);
-        // dd($owner);
-        return view('admin.owners.edit', compact('owner'));
-    }
+  /**
+   * Display the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function show($id)
+  {
+    //
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        $owner = Owner::findOrFail($id);
-        $owner->name = $request->name;
-        $owner->email = $request->email;
-        $owner->password = Hash::make($request->password);
-        $owner->save();
+  /**
+   * Show the form for editing the specified resource.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function edit($id)
+  {
+    $owner = Owner::findOrFail($id);
+    // dd($owner);
+    return view('admin.owners.edit', compact('owner'));
+  }
 
-        return redirect()
-            ->route('admin.owners.index')
-            ->with([
-                'message' => 'オーナー情報を更新しました。',
-                'status' => 'info'
-            ]);
-    }
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    $owner = Owner::findOrFail($id);
+    $owner->name = $request->name;
+    $owner->email = $request->email;
+    $owner->password = Hash::make($request->password);
+    $owner->save();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        Owner::findOrFail($id)->delete();
+    return redirect()
+      ->route('admin.owners.index')
+      ->with([
+        'message' => 'オーナー情報を更新しました。',
+        'status' => 'info'
+      ]);
+  }
 
-        return redirect()
-            ->route('admin.owners.index')
-            ->with([
-                'message' => 'オーナー情報を削除しました。',
-                'status' => 'alert'
-            ]);
-    }
-    public function expiredOwnerIndex()
-    {
-        $expiredOwners = Owner::onlyTrashed()->get();
-        return view('admin.expired-owners', compact('expiredOwners'));
-    }
-    public function expiredOwnerDestroy($id)
-    {
-        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
-        return redirect()
-        ->route('admin.expired-owners.index')
-        ->with([
-            'message' => 'オーナー情報を完全に削除しました。',
-            'status' => 'alert'
-        ]);
-    }
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    Owner::findOrFail($id)->delete();
+
+    return redirect()
+      ->route('admin.owners.index')
+      ->with([
+        'message' => 'オーナー情報を削除しました。',
+        'status' => 'alert'
+      ]);
+  }
+  public function expiredOwnerIndex()
+  {
+    $expiredOwners = Owner::onlyTrashed()->get();
+    return view('admin.expired-owners', compact('expiredOwners'));
+  }
+  public function expiredOwnerDestroy($id)
+  {
+    Owner::onlyTrashed()->findOrFail($id)->forceDelete();
+    return redirect()
+      ->route('admin.expired-owners.index')
+      ->with([
+        'message' => 'オーナー情報を完全に削除しました。',
+        'status' => 'alert'
+      ]);
+  }
 }
