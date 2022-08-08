c7b9086c8ef2411da968f5c79827afffc2f886fc
Mikiko Noro <miriks818@gmail.com>
2022-08-09 00:51:41 +0900

    productEdit

--- app/Http/Controllers/Owner/ProductController.php
+++ app/Http/Controllers/Owner/ProductController.php
@@ -15,167 +15,169 @@ use App\Models\Owner;
 
 class ProductController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth:owners');
+  public function __construct()
+  {
+    $this->middleware('auth:owners');
 
-        $this->middleware(function ($request, $next) {
+    $this->middleware(function ($request, $next) {
 
-            $id = $request->route()->parameter('product'); //shopのid取得
-            if (!is_null($id)) {
-                $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
-                $productId = (int)$productsOwnerId;
-                if ($productId !== Auth::id()) {
-                    abort(404);
-                }
-            }
-            return $next($request);
-        });
-    }
-    public function index()
-    {
-        // $products = Owner::findOrFail(Auth::id())->shop->product;
-        $ownerInfo = Owner::with('shop.product.imageFirst')
-        ->where('id', Auth::id())->get();
-        // dd($ownerInfo);
-        // foreach($ownerInfo as $owner){
-        //     // dd($owner->shop->product);
-        //     foreach($owner->shop->product as $product){
-        //         dd($product->imageFirst->filename);
-        //     }
-        // }
+      $id = $request->route()->parameter('product'); //shopのid取得
+      if (!is_null($id)) {
+        $productsOwnerId = Product::findOrFail($id)->shop->owner->id;
+        $productId = (int)$productsOwnerId;
+        if ($productId !== Auth::id()) {
+          abort(404);
+        }
+      }
+      return $next($request);
+    });
+  }
+  public function index()
+  {
+    // $products = Owner::findOrFail(Auth::id())->shop->product;
+    $ownerInfo = Owner::with('shop.product.imageFirst')
+      ->where('id', Auth::id())->get();
+    // dd($ownerInfo);
+    // foreach($ownerInfo as $owner){
+    //     // dd($owner->shop->product);
+    //     foreach($owner->shop->product as $product){
+    //         dd($product->imageFirst->filename);
+    //     }
+    // }
 
-        return view('owner.products.index',
-        compact('ownerInfo'));
-    }
+    return view(
+      'owner.products.index',
+      compact('ownerInfo')
+    );
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        $shops = Shop::where('owner_id', Auth::id())
-        ->select('id', 'name')
-        ->get();
+  /**
+   * Show the form for creating a new resource.
+   *
+   * @return \Illuminate\Http\Response
+   */
+  public function create()
+  {
+    $shops = Shop::where('owner_id', Auth::id())
+      ->select('id', 'name')
+      ->get();
 
-        $images = Image::where('owner_id', Auth::id())
-        ->select('id', 'title', 'filename')
-        ->orderBy('updated_at', 'desc')
-        ->get();
+    $images = Image::where('owner_id', Auth::id())
+      ->select('id', 'title', 'filename')
+      ->orderBy('updated_at', 'desc')
+      ->get();
 
-        $categories = PrimaryCategory::with('secondary')
-        ->get();
+    $categories = PrimaryCategory::with('secondary')
+      ->get();
 
-        return view('owner.products.create',
-        compact('shops', 'images', 'categories'));
+    return view(
+      'owner.products.create',
+      compact('shops', 'images', 'categories')
+    );
+  }
 
-    }
+  /**
+   * Store a newly created resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @return \Illuminate\Http\Response
+   */
+  public function store(Request $request)
+  {
+    $request->validate([
+      'name' => 'required|string|max:50',
+      'information' => 'required|string|max:1000',
+      'price' => 'required|integer',
+      'sort_order' => 'nullable|integer',
+      'quantity' => 'required|integer',
+      'shop_id' => 'required|exists:shops,id',
+      'category' => 'required|exists:secondary_categories,id',
+      'image1' => 'nullable|exists:images,id',
+      'image2' => 'nullable|exists:images,id',
+      'image3' => 'nullable|exists:images,id',
+      'image4' => 'nullable|exists:images,id',
+      'is_selling' => 'required'
+    ]);
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:50',
-            'information' => 'required|string|max:1000',
-            'price' => 'required|integer',
-            'sort_order' => 'nullable|integer',
-            'quantity' => 'required|integer',
-            'shop_id' => 'required|exists:shops,id',
-            'category' => 'required|exists:secondary_categories,id',
-            'image1' => 'nullable|exists:images,id',
-            'image2' => 'nullable|exists:images,id',
-            'image3' => 'nullable|exists:images,id',
-            'image4' => 'nullable|exists:images,id',
-            'is_selling' => 'required'
-          ]);
+    try {
+      DB::transaction(function () use ($request) {
+        $product = Product::create([
+          'name' => $request->name,
+          'information' => $request->information,
+          'price' => $request->price,
+          'sort_order' => $request->sort_order,
+          'shop_id' => $request->shop_id,
+          'secondary_category_id' => $request->category,
+          'image1' => $request->image1,
+          'image2' => $request->image2,
+          'image3' => $request->image3,
+          'image4' => $request->image4,
+          'is_selling' => $request->is_selling
+        ]);
 
-          try {
-            DB::transaction(function () use ($request) {
-              $product = Product::create([
-                'name' => $request->name,
-                'information' => $request->information,
-                'price' => $request->price,
-                'sort_order' => $request->sort_order,
-                'shop_id' => $request->shop_id,
-                'secondary_category_id' => $request->category,
-                'image1' => $request->image1,
-                'image2' => $request->image2,
-                'image3' => $request->image3,
-                'image4' => $request->image4,
-                'is_selling' => $request->is_selling
-              ]);
-
-              Stock::create([
-                'product_id' => $product->id,
-                'type' => 1,
-                'quantity' => $request->quantity,
-              ]);
-            }, 2);
-          } catch (Throwable $e) {
-            Log::error($e);
-            throw $e;
-          }
-
-          return redirect()
-            ->route('owner.products.index')
-            ->with([
-              'message' => '商品を登録ました。',
-              'status' => 'info'
-            ]);
-
-
-
+        Stock::create([
+          'product_id' => $product->id,
+          'type' => 1,
+          'quantity' => $request->quantity,
+        ]);
+      }, 2);
+    } catch (Throwable $e) {
+      Log::error($e);
+      throw $e;
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+    return redirect()
+      ->route('owner.products.index')
+      ->with([
+        'message' => '商品を登録ました。',
+        'status' => 'info'
+      ]);
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
+  public function edit($id)
+  {
+    $product = Product::findOrFail($id);
+    $quantity = Stock::where('product_id', $product->id)
+      ->sum('quantity');
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
+    $shops = Shop::where('owner_id', Auth::id())
+      ->select('id', 'name')
+      ->get();
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
+    $images = Image::where('owner_id', Auth::id())
+      ->select('id', 'title', 'filename')
+      ->orderBy('updated_at', 'desc')
+      ->get();
+
+    $categories = PrimaryCategory::with('secondary')
+      ->get();
+
+    return view(
+      'owner.products.edit',
+      compact('product', 'quantity', 'shops', 'images', 'categories')
+    );
+  }
+
+  /**
+   * Update the specified resource in storage.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function update(Request $request, $id)
+  {
+    //
+  }
+
+  /**
+   * Remove the specified resource from storage.
+   *
+   * @param  int  $id
+   * @return \Illuminate\Http\Response
+   */
+  public function destroy($id)
+  {
+    //
+  }
 }
