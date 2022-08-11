7d921b01a46a441660ae72f5c31571f2fc151f1b
Mikiko Noro <miriks818@gmail.com>
2022-08-11 14:50:43 +0900

    constant

--- resources/views/owner/products/edit.blade.php
+++ resources/views/owner/products/edit.blade.php
@@ -45,7 +45,7 @@
               </div>
               <div class="p-2 w-1/2 mx-auto">
                 <div class="relative">
-                  <label for="current_quantity" class="leading-7 text-sm text-gray-600">初期在庫※必須</label>
+                  <label for="current_quantity" class="leading-7 text-sm text-gray-600">現在の在庫※必須</label>
                   <input type="hidden" id="current_quantity" name="current_quantity" value="{{ $quantity }}" required>
                   <div
                     class="w-full bg-gray-100 bg-opacity-50 rounded text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
@@ -116,10 +116,10 @@
               <div class="p-2 w-1/2 mx-auto">
                 <div class="relative flex justify-around">
                   <div>
-                    <input type="radio" name="is_selling" value="1" class="mr-2" checked>販売中
+                    <input type="radio" name="is_selling" value="{{ \Constant::PRODUCT_LIST['add'] }}" class="mr-2" checked>販売中
                   </div>
                   <div>
-                    <input type="radio" name="is_selling" value="0" class="mr-2">停止中
+                    <input type="radio" name="is_selling" value="{{ \Constant::PRODUCT_LIST['add'] }}" class="mr-2">停止中
                   </div>
                 </div>
               </div>
