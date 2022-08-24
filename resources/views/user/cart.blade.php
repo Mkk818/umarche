3ead03ba33cfdddade0fb8961eb9d7fc53e56c8f
Mikiko Noro <miriks818@gmail.com>
2022-08-24 16:52:03 +0900

    cartIndexDelete

--- resources/views/user/cart.blade.php
+++ resources/views/user/cart.blade.php
@@ -23,12 +23,24 @@
             <div class="md:w-3/12 flex justify-around">
               <div>{{ $product->pivot->quantity }}個</div>
               <div>{{ number_format($product->pivot->quantity * $product->price ) }}
-                <span class="text-sm text-gray-700">円(税込)</span></div>
+                <span class="text-sm text-gray-700">円(税込)</span>
+              </div>
             </div>
-            <div class="md:w-2/12">削除ボタン</div>
+            <div class="md:w-2/12">
+              <form method="post" action="{{route('user.cart.delete', ['item' => $product->id ])}}">
+                @csrf
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                    stroke="currentColor" class="w-6 h-6">
+                    <path stroke-linecap="round" stroke-linejoin="round"
+                      d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                </button>
+            </div>
+            </form>
           </div>
           @endforeach
-          合計金額：{{ $totalPrice }}
+          {{-- 合計金額：{{ $totalPrice }} --}}
           @else
           カートの商品が入っていません。
           @endif
