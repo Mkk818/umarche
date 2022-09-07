3e88cd1cf04f791b45508052de888cb6c3cb9247
Mikiko Noro <miriks818@gmail.com>
2022-09-07 10:24:49 +0900

    Tweakindex

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -46,15 +46,17 @@
             <div>
             <span class="text-sm">表示件数</span><br>
             <select name="pagination" id="pagination">
-              <option value="20" @if(\Request::get('pagination') === '20') selected @endif>20件</option>
-              <option value="50" @if(\Request::get('pagination') === '50') selected @endif>50件</option>
-              <option value="100" @if(\Request::get('pagination') === '100') selected @endif>100件</option>
+              <option value="20" @if(\Request::get('pagination') === '20') selected @endif>20件
+              </option>
+              <option value="50" @if(\Request::get('pagination') === '50') selected @endif>50件
+              </option>
+              <option value="100" @if(\Request::get('pagination') === '100') selected @endif>100件
+              </option>
             </select>
             </div>
           </div>
+        </div>
         </form>
-    </div>
-    </div>
   </x-slot>
 
   <div class="py-12">
